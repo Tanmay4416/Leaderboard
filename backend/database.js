cc0efe28b0ce8bf81ae63c7fd42052a049eabf6a
Tanmay4416 <tanmayk2001@gmail.com>
2024-02-28 19:25:09 +0530

    changes to .env

--- backend/database.js
+++ backend/database.js
@@ -7,7 +7,8 @@ let pool = mysql.createPool({
     host            : process.env.MYSQL_HOST,
     user            : process.env.MYSQL_USER,
     password        : process.env.MYSQL_PASSWORD,
-    database        : process.env.MYSQL_DATABASE
+    database        : process.env.MYSQL_DATABASE,
+    port            : 48804
 }).promise();
 
 
