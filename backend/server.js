0922a395a1696af1a5205f258ba7191cf5f57651
Tanmay4416 <tanmayk2001@gmail.com>
2024-02-29 11:31:46 +0530

    final changes

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const str = `<h4>API for this week's leaderboard :  <em>https://leaderboard-1gbs.onrender.com/leaderboard</em></h4> <h4>API for previous week's leaderboard by country :  <em>https://leaderboard-1gbs.onrender.com/leaderboard?country={country's 2 letter ISO code}</em><ul><li style="color:MediumSeaGreen"><p style="color:black">example : </p><em>https://leaderboard-1gbs.onrender.com/leaderboard?country=IN</em></li></ul></h4> <h4>API for User's rank :  <em>https://leaderboard-1gbs.onrender.com/leaderboard/rank?id={User_Id}</em><ul><li style="color:DodgerBlue;"}><p style="color:black">example : </p><em>https://leaderboard-1gbs.onrender.com/leaderboard/rank?id=00148f01-15ae-4fdf-92b0-0cf02c79901c</em></li></ul></h4>`;
+const str = `<h4>API for this week's leaderboard :  <em>https://leaderboard-1gbs.onrender.com/leaderboard</em></h4> 
+<h4>API for previous week's leaderboard by country :  <em>https://leaderboard-1gbs.onrender.com/leaderboard?country={country's 2 letter ISO code}</em>
+<ul><li style="color:MediumSeaGreen"><p style="color:black">example : </p><em>https://leaderboard-1gbs.onrender.com/leaderboard?country=IN</em>
+</li><li>Country codes in database : IN,US,GB,CA,AU,DE,FR,IT,JP,CN,BR</li></ul></h4> 
+<h4>API for User's rank :  <em>https://leaderboard-1gbs.onrender.com/leaderboard/rank?id={User_Id}</em><ul><li style="color:DodgerBlue;"}>
+<p style="color:black">example : </p><em>https://leaderboard-1gbs.onrender.com/leaderboard/rank?id=00148f01-15ae-4fdf-92b0-0cf02c79901c</em></li></ul></h4>`;
 
 app.get('/', (req,res) => {
     res.send(str);
@@ -52,7 +57,7 @@ app.get('/leaderboard/rank', (req,res) => {
     getUserRankById(id).then((result) => {
         if(result) {
             console.log('Rank: ',result[0]['COUNT(*)']);
-            res.send(`Rank : ${result[0]['COUNT(*)']}`);
+            res.send(`Rank : ${result[0]['COUNT(*)']} | UID : ${id}`);
         } else {
             res.status(400).json('no such user found');
         }
